8a3ae397767bb24a98229afddc266535b645f06d
Ankit Tewari <58264046+4nkt@users.noreply.github.com>
2020-01-06 23:59:41 +0530

    Generate a serializable value with jest snapshots

---
+++ packages/jest-cypress-react-babel-webpack/src/shared/__tests__/calculator-display.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import CalculatorDisplay from '../calculator-display'
+
+test('renders', () => {
+  const {container} = render(<CalculatorDisplay value="0" />)
+  expect(container.firstChild).toMatchInlineSnapshot(`
+    <div
+      class="css-lq9ahq-calculator-display--CalculatorDisplay"
+    >
+      <div
+        class="autoScalingText"
+        style="transform: scale(1,1);"
+      >
+        0
+      </div>
+    </div>
+  `)
+})
