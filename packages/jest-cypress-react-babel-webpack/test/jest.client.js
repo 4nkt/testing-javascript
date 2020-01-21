2f7a1bc076805e5fcc00a6be05d679abe0d63318
Ankit Tewari <58264046+4nkt@users.noreply.github.com>
2020-01-22 00:06:20 +0530

    Add server config

---
+++ packages/jest-cypress-react-babel-webpack/test/jest.client.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  ...require('./jest-common'),
+  coverageDirectory: path.join(__dirname, '../coverage/client'),
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  snapshotSerializers: ['jest-emotion'],
+  coverageThreshold: {
+    global: {
+      statements: 15,
+      branches: 10,
+      functions: 15,
+      lines: 15,
+    },
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+    },
+  },
+}
