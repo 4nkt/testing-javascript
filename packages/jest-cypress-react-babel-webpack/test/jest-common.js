1fa499d9070d32d38ef3daec535730f134a9bf39
Ankit Tewari <58264046+4nkt@users.noreply.github.com>
2020-01-28 16:58:58 +0530

    Filter which tests are run with typeahead support in jest watch mode

--- packages/jest-cypress-react-babel-webpack/test/jest-common.js
+++ packages/jest-cypress-react-babel-webpack/test/jest-common.js
@@ -12,5 +12,9 @@ module.exports = {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./style-mock.js'),
   },
-  watchPlugins: ['jest-watch-select-projects'],
+  watchPlugins: [
+    'jest-watch-select-projects',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 }
