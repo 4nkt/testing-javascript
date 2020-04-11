a33683d4c32ffb1a8205e13dd4230f6280ce10ed
Ankit Tewari <58264046+4nkt@users.noreply.github.com>
2020-04-11 21:08:56 +0530

    Use dom testing library with hyperapp

---
+++ packages/dom-testing-library/hyperapp.test.js
@@ -0,0 +1,36 @@
+/** @jsx hyperapp.h */
+import '@testing-library/jest-dom/extend-expect'
+import * as hyperapp from 'hyperapp'
+import {getQueriesForElement, wait, fireEvent} from '@testing-library/dom'
+
+const state = {count: 0}
+
+const view = state => (
+  <div>
+    <button onclick={s => ({count: s.count + 1})}>{state.count}</button>
+  </div>
+)
+
+async function render({
+  state,
+  view,
+  container = document.createElement('div'),
+}) {
+  hyperapp.app({init: state, view, node: container})
+  document.body.appendChild(container)
+  await wait(() => expect(container).not.toBeEmpty())
+  return {
+    container,
+    ...getQueriesForElement(container),
+  }
+}
+
+test('renders a counter', async () => {
+  const {findByText} = await render({state, view})
+  const counter = await findByText('0')
+  fireEvent.click(counter)
+  await wait(() => expect(counter).toHaveTextContent('1'))
+
+  fireEvent.click(counter)
+  await wait(() => expect(counter).toHaveTextContent('2'))
+})
