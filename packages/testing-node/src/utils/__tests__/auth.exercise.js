326a11a812bcdb52be0a6524604037c08929f088
Ankit Tewari <58264046+4nkt@users.noreply.github.com>
2020-04-27 08:18:31 +0530

    Improve error messages by generating test titles

--- packages/testing-node/src/utils/__tests__/auth.exercise.js
+++ packages/testing-node/src/utils/__tests__/auth.exercise.js
@@ -2,15 +2,26 @@
 
 import {isPasswordAllowed} from '../auth'
 
-test('isPasswordAllowed returns true for valid passwords', () => {
-  expect(isPasswordAllowed('!aBc123')).toBe(true);
-});
+describe('isPasswordAllowed only allows some passwords', () => {
+  const allowedPasswords = ['!aBc123']
+  const disallowedPasswords = [
+    'a2c!',
+    '123456!',
+    'ABCdef!',
+    'abc123!',
+    'ABC123!',
+    'ABCdef123',
+  ]
 
-test('isPasswordAllowed returns false for invalid passwords', () => {
-  expect(isPasswordAllowed('a2c!')).toBe(false);
-  expect(isPasswordAllowed('123456!')).toBe(false);
-  expect(isPasswordAllowed('ABCdef!')).toBe(false);
-  expect(isPasswordAllowed('abc123!')).toBe(false);
-  expect(isPasswordAllowed('ABC123!')).toBe(false);
-  expect(isPasswordAllowed('ABCdef123')).toBe(false);
-});
+  allowedPasswords.forEach(password => {
+    test(`allows ${password}`, () => {
+      expect(isPasswordAllowed(password)).toBe(true)
+    })
+  })
+
+  disallowedPasswords.forEach(password => {
+    test(`disallows ${password}`, () => {
+      expect(isPasswordAllowed(password)).toBe(false)
+    })
+  })
+})
