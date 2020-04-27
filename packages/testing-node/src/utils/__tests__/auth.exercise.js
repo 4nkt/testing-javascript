5dd3b26400fcfc5ee86beff187c97b9742ddba24
Ankit Tewari <58264046+4nkt@users.noreply.github.com>
2020-04-27 09:03:57 +0530

    Use jest-in-case to reduce duplication and improve test titles

--- packages/testing-node/src/utils/__tests__/auth.exercise.js
+++ packages/testing-node/src/utils/__tests__/auth.exercise.js
@@ -1,27 +1,43 @@
 // Testing Pure Functions
 
+import cases from 'jest-in-case'
 import {isPasswordAllowed} from '../auth'
 
-describe('isPasswordAllowed only allows some passwords', () => {
-  const allowedPasswords = ['!aBc123']
-  const disallowedPasswords = [
-    'a2c!',
-    '123456!',
-    'ABCdef!',
-    'abc123!',
-    'ABC123!',
-    'ABCdef123',
-  ]
+cases(
+  'isPasswordAllowed: valid passwords',
+  ({password}) => {
+    expect(isPasswordAllowed(password)).toBe(true)
+  },
+  {
+    'valid password': {
+      password: '!aBc123',
+    },
+  },
+)
 
-  allowedPasswords.forEach(password => {
-    test(`allows ${password}`, () => {
-      expect(isPasswordAllowed(password)).toBe(true)
-    })
-  })
-
-  disallowedPasswords.forEach(password => {
-    test(`disallows ${password}`, () => {
-      expect(isPasswordAllowed(password)).toBe(false)
-    })
-  })
-})
+cases(
+  'isPasswordAllowed: invalid passwords',
+  ({password}) => {
+    expect(isPasswordAllowed(password)).toBe(false)
+  },
+  {
+    'too short': {
+      password: 'a2c!',
+    },
+    'no letters': {
+      password: '123456!',
+    },
+    'no numbers': {
+      password: 'ABCdef!',
+    },
+    'no uppercase letters': {
+      password: 'abc123!',
+    },
+    'no lowercase letters': {
+      password: 'ABC123!',
+    },
+    'no non-alphanumeric characters': {
+      password: 'ABCdef123',
+    },
+  },
+)
