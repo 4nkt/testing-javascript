84cb27e9fdd1deee16dedd9e1cfa280b410a5517
Ankit Tewari <58264046+4nkt@users.noreply.github.com>
2020-05-07 02:46:43 +0530

    Unit test business logic of a controller’s middleware

--- packages/testing-node/src/routes/__tests__/list-items-controller.exercise.js
+++ packages/testing-node/src/routes/__tests__/list-items-controller.exercise.js
@@ -3,13 +3,16 @@
 import {
   buildRes,
   buildReq,
+  buildNext,
   buildUser,
   buildBook,
   buildListItem,
 } from 'utils/generate'
+import * as listItemsDB from '../../db/list-items'
 import * as booksDB from '../../db/books'
 import * as listItemsController from '../list-items-controller'
 
+jest.mock('../../db/list-items')
 jest.mock('../../db/books')
 
 beforeEach(() => {
@@ -53,4 +56,25 @@ test('createListItem returns a 400 error if no bookId is provided', async () => 
     ]
   `)
   expect(res.json).toHaveBeenCalledTimes(1)
+})
+
+test('setListItem sets the listItem on the req', async () => {
+  const user = buildUser()
+  const listItem = buildListItem({ownerId: user.id})
+
+  listItemsDB.readById.mockResolvedValueOnce(listItem)
+
+  const req = buildReq({user, params: {id: listItem.id}})
+  const res = buildRes()
+  const next = buildNext()
+
+  await listItemsController.setListItem(req, res, next)
+
+  expect(listItemsDB.readById).toHaveBeenCalledWith(listItem.id)
+  expect(listItemsDB.readById).toHaveBeenCalledTimes(1)
+
+  expect(next).toHaveBeenCalledWith(/* nothing */)
+  expect(next).toHaveBeenCalledTimes(1)
+
+  expect(req.listItem).toBe(listItem)
 })
