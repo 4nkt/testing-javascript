d86d149afb42e40c762abc6cafb82764977e5ce6
Ankit Tewari <58264046+4nkt@users.noreply.github.com>
2020-05-29 00:00:12 +0530

    Use snapshots to assert on server error responses

--- packages/testing-node/src/__tests__/auth.exercise.js
+++ packages/testing-node/src/__tests__/auth.exercise.js
@@ -3,7 +3,7 @@
 import axios from 'axios'
 import {resetDb} from 'utils/db-utils'
 import * as generate from 'utils/generate'
-import {getData, handleRequestFailure} from 'utils/async'
+import {getData, handleRequestFailure, resolve} from 'utils/async'
 import startServer from '../start'
 
 let api, server
@@ -41,4 +41,17 @@ test('auth flow', async () => {
     },
   })
   expect(mData.user).toEqual(lData.user)
+})
+
+test('username must be unique', async () => {
+  const {username, password} = generate.loginForm()
+  await api.post('auth/register', {username, password})
+
+  const error = await api
+    .post('auth/register', {username, password})
+    .catch(resolve)
+
+  expect(error).toMatchInlineSnapshot(
+    `[Error: 400: {"message":"username taken"}]`,
+  )
 })
