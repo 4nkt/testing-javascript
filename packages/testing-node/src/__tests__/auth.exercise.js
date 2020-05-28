5bb6737c3a96a8fb18cf96e2ea80aad90bccd84a
Ankit Tewari <58264046+4nkt@users.noreply.github.com>
2020-05-28 23:40:49 +0530

    Improve error messages with an axios interceptor

--- packages/testing-node/src/__tests__/auth.exercise.js
+++ packages/testing-node/src/__tests__/auth.exercise.js
@@ -3,6 +3,7 @@
 import axios from 'axios'
 import {resetDb} from 'utils/db-utils'
 import * as generate from 'utils/generate'
+import {getData, handleRequestFailure} from 'utils/async'
 import startServer from '../start'
 
 let server
@@ -17,27 +18,28 @@ beforeEach(() => resetDb())
 
 const baseURL = 'http://localhost:8000/api'
 const api = axios.create({baseURL})
+api.interceptors.response.use(getData, handleRequestFailure)
 
 test('auth flow', async () => {
   const {username, password} = generate.loginForm()
 
   // register
-  const rResult = await api.post('auth/register', {username, password})
-  expect(rResult.data.user).toEqual({
+  const rData = await api.post('auth/register', {username, password})
+  expect(rData.user).toEqual({
     token: expect.any(String),
     id: expect.any(String),
     username,
   })
 
   // login
-  const lResult = await api.post('auth/login', {username, password})
-  expect(lResult.data.user).toEqual(rResult.data.user)
+  const lData = await api.post('auth/login', {username, password})
+  expect(lData.user).toEqual(rData.user)
 
   // authenticated request
-  const mResult = await api.get('auth/me', {
+  const mData = await api.get('auth/me', {
     headers: {
-      Authorization: `Bearer ${lResult.data.user.token}`,
+      Authorization: `Bearer ${lData.user.token}`,
     },
   })
-  expect(mResult.data.user).toEqual(lResult.data.user)
+  expect(mData.user).toEqual(lData.user)
 })
