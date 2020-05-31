dfe87333676aa8a382043c3b16138d29c9dfd9ed
Ankit Tewari <58264046+4nkt@users.noreply.github.com>
2020-05-31 21:06:35 +0530

    Fix jest client configuration

--- packages/jest-cypress-react-babel-webpack/test/jest.client.js
+++ packages/jest-cypress-react-babel-webpack/test/jest.client.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 module.exports = {
   ...require('./jest-common'),
   displayName: 'client',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   snapshotSerializers: ['jest-emotion'],
+  rootDir: path.join(__dirname, '../src'),
 }
